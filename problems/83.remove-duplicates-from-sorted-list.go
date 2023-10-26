7bd440999f984a7d03e36916eb6f9d7ff7ba4ee0
grahovsky <grahovsky@gmail.com>
2023-10-26 23:10:29 +0300

    83 problem solved

---
+++ problems/83.remove-duplicates-from-sorted-list.go
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=83 lang=golang
+ *
+ * [83] Remove Duplicates from Sorted List
+ */
+
+package problems
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+	cur := head
+	for cur != nil && cur.Next != nil {
+		if cur.Next.Val == cur.Val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+	return head
+}
+
+// @lc code=end
---
+++ problems/83_test.go
@@ -0,0 +1,30 @@
+package problems
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test83(t *testing.T) {
+	tests := []struct {
+		input1 *ListNode
+		result *ListNode
+	}{
+		{
+			input1: &ListNode{Val: 1, Next: &ListNode{Val: 1, Next: &ListNode{Val: 2}}},
+			result: &ListNode{Val: 1, Next: &ListNode{Val: 2}},
+		},
+		{
+			input1: &ListNode{Val: 1, Next: &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 3}}}}},
+			result: &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3}}},
+		},
+	}
+	for _, tc := range tests {
+		tc := tc
+		t.Run("test 70", func(t *testing.T) {
+			result := deleteDuplicates(tc.input1)
+			require.Equal(t, tc.result, result)
+		})
+	}
+}
