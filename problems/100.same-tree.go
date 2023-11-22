fbbcdaf006e2ecb634df1c83b3c771cc04e985a8
Grakhovsky <grahovsky@gmail.com>
2023-11-22 08:53:03 +0300

    refactor

--- problems/100.same-tree.go
+++ problems/100.same-tree.go
@@ -16,13 +16,11 @@ package problems
  * }
  */
 func isSameTree(p *TreeNode, q *TreeNode) bool {
-	if p == nil && q == nil {
-		return true
-	} else if p == nil || q == nil || p.Val != q.Val {
-		return false
+	if p == nil || q == nil {
+		return q == p
 	}
 
-	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+	return p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
 }
 
 // @lc code=end
